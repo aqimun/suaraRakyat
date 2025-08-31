77f3f9ac9e065ff4820676bd953b8794fd7facc6
Harits Althaf <119770292+aqimun@users.noreply.github.com>
2025-08-31 21:11:57 +0700

    initial commit

---
+++ src/test/java/com/tejasTanra/suaraRakyat/SuaraRakyatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejasTanra.suaraRakyat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuaraRakyatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
